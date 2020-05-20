50e1fbb429aeb4a20c2bc5210a0ee29b9b0afee0
Deandrew Moore <deamoore07@gmail.com>
2020-05-20 13:03:18 -0500

    final changes to display posts. definitely not the best implementation but just tried to hack something together

--- Frontend/app.py
+++ Frontend/app.py
@@ -18,6 +18,9 @@ profileData = cursor.fetchall()
 cursor.execute("Select * from user WHERE userID = 10")
 userData = cursor.fetchall()
 
+cursor.execute("Select text,image,postDate, firstname from postTable join user where userID between 1 and 15 limit 15")
+postData = cursor.fetchall()
+
 
 bioList = []
 schoolList = []
@@ -36,6 +39,15 @@ for i in userData:
     fnameList.append(i[2])
     lnameList.append(i[3])
 
+postName =[]
+postText = []
+postImage = []
+postDate = []
+for n in postData:
+    postName.append(n[3])
+    postText.append(n[0])
+    postImage.append(n[1])
+    postDate.append(n[2])
 
 
 cursor.close()
@@ -63,7 +75,7 @@ def profile():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', fname0 = postName[0], fname1 = postName[1],fname2 = postName[2], posttext0 = postText[0],posttext1 = postText[1],posttext2 = postText[2],pdate0 = postDate[0],pdate1 = postDate[1],pdate2 = postDate[2])
 
 if __name__ == "__main__":
     app.run(debug=True)
